ff624d93df62d61977d359b9f94c2a63adcf84d5
nknavkal <nknavkal@gmail.com>
2022-10-14 19:53:43 -0400

    swapped out timestamps for timestampAtIndex, added notice to dissuade users from calling useless functions

--- src/ECOxEmployeeLockup.sol
+++ src/ECOxEmployeeLockup.sol
@@ -4,9 +4,17 @@ import {ECOxLockupVault} from "./ECOxLockupVault.sol";
 import {IECOx} from "./interfaces/IECOx.sol";
 import {IERC20Upgradeable} from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/IERC20Upgradeable.sol";
 
+/** 
+ * @notice ECOxEmployeeLockup contract holds ECOx for employees until a cliff date has passed. 
+ * Notably it is initialized with only one timestamp (cliff date) and one initial token amount (0).
+ * As a result, the methods found in ChunkedVestingVaultArgs will not provide useful information
+ * and any methods referring to amounts, timestamps and chunks will reflect the fact that there is
+ * only one of each. 
+ */
 contract ECOxEmployeeLockup is ECOxLockupVault {
+
     function vestedOn(uint256 timestamp) public view override returns (uint256 amount) {
-        return timestamp >= this.timestamps()[0] ? token().balanceOf(address(this)) + IERC20Upgradeable(lockup).balanceOf(address(this)) : 0;
+        return timestamp >= this.timestampAtIndex(0) ? token().balanceOf(address(this)) + IERC20Upgradeable(lockup).balanceOf(address(this)) : 0;
     }
 
     function onClaim(uint256 amount) internal override {
