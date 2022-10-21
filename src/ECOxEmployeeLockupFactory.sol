70a19e159a9b63c606813ad6459d0d83fc280ec3
nknavkal <nknavkal@gmail.com>
2022-10-21 13:57:55 -0400

    initial commit + made some changes to descripions and annotations

--- src/ECOxEmployeeLockupFactory.sol
+++ src/ECOxEmployeeLockupFactory.sol
@@ -21,8 +21,8 @@ contract ECOxEmployeeLockupFactory is IVestingVaultFactory {
      * @notice Creates a new vesting vault
      * @param token The ERC20 token to vest over time
      * @param beneficiary The address who will receive tokens over time
-     * @param timestamp The cliff timestamp at which tokens vest
      * @param admin The address that can claw back unvested funds
+     * @param timestamp The cliff timestamp at which tokens vest
      * @return The address of the ECOxEmployeeLockup contract created
      */
 
