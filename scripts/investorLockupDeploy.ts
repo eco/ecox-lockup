28d6113b388bdde9b2faeb412f4380a8bb2651e2
nknavkal <nknavkal@gmail.com>
2023-01-31 18:52:17 -0500

    tested deploy script on goerli; it works + can claim successfully

--- scripts/investorLockupDeploy.ts
+++ scripts/investorLockupDeploy.ts
@@ -6,7 +6,6 @@ const RPCURL = process.env.RPCURL || ''
 const signerPK = process.env.PK || ''
 // const TOKEN = process.env.TOKEN || ''
 const POLICY = process.env.POLICY || ''
-const LOCKUP_DURATION = process.env.DURATION || ''
 const ADMIN = process.env.ADMIN || ''
 
 let ID_ECOX = ethers.utils.solidityKeccak256( ['string'],['ECOx'])
@@ -36,40 +35,54 @@ async function deployVaultFactory() {
     let implFactory = new ethers.ContractFactory(lockupVaultABI.abi, lockupVaultABI.bytecode, wallet)
     let lockupVaultImpl = await implFactory.deploy()
     await lockupVaultImpl.deployTransaction.wait()
-    console.log(`lockupVaultimpl: ${lockupVaultImpl.address}`)
+    console.log(`lockupVaultImpl: ${lockupVaultImpl.address}`)
 
     let policy = new ethers.Contract(POLICY, policyABI.abi, wallet)
     let ecoX = new ethers.Contract(await policy.policyFor(ID_ECOX), ecoxABI.abi, wallet)
     let ecoXStaking = new ethers.Contract(await policy.policyFor(ID_ECOX_STAKING), ecoxStakingABI.abi, wallet)
 
     let lockupVaultFactoryFactory = new ethers.ContractFactory(lockupFactoryABI.abi, lockupFactoryABI.bytecode, wallet)
-    let lockupVaultFactory = await lockupVaultFactoryFactory.deploy(lockupVaultImpl, ecoX, ecoXStaking)
+    let lockupVaultFactory = await lockupVaultFactoryFactory.deploy(lockupVaultImpl.address, ecoX.address, ecoXStaking.address)
     await lockupVaultFactory.deployTransaction.wait()
-    console.log(`lockupVaultFactory: ${lockupVaultImpl.address}`)
+    console.log(`lockupVaultFactory: ${lockupVaultFactory.address}`)
 }
 
-async function launchVaults(lockupVaultFactory: any, lockupCsvFilename: string) {
+async function launchVaults(lockupVaultFactoryAddress: any, lockupCsvFilename: string) {
     let provider: ethers.providers.BaseProvider = new ethers.providers.JsonRpcProvider(RPCURL)
     let wallet: ethers.Wallet = new ethers.Wallet(signerPK, provider)
+    let policy = new ethers.Contract(POLICY, policyABI.abi, wallet)
+    let ecoX = new ethers.Contract(await policy.policyFor(ID_ECOX), ecoxABI.abi, wallet)
+    let lockupVaultFactory = new ethers.Contract(lockupVaultFactoryAddress, lockupFactoryABI.abi, wallet)
 
     // populates lockupInfos
     await processInputs(lockupCsvFilename)
+    console.log(lockupInfos)
 
     let tx
     let receipt
     for (let i = 0; i < lockupInfos.length; i++) {
         let info: lockupInfo = lockupInfos[i]
         try {
-            console.log(`trying to deploy lockup for ${info.beneficiary}`)
-            tx = await lockupVaultFactory.connect(wallet).createVault(info.beneficiary, ADMIN, info.amounts, info.timestamps)
+            let totalVaultECOx:number = 0
+            for (let i = 0; i < info.amounts.length; i++) {
+                totalVaultECOx += parseInt(info.amounts[i])
+            }
+            console.log(`approving ${totalVaultECOx} ecox for ${info.beneficiary}`)
+            tx = await ecoX.approve(lockupVaultFactoryAddress, totalVaultECOx.toString())
             receipt = await tx.wait()
             if (receipt.status === 1) {
-                console.log(`deployed lockup for ${info.beneficiary}`)
-                await new Promise(r => setTimeout(r, 30000));
-                continue
+                console.log(`approved for ${info.beneficiary}, trying to deploy lockup`)
+                tx = await lockupVaultFactory.connect(wallet).createVault(info.beneficiary, ADMIN, info.amounts, info.timestamps)
+                receipt = await tx.wait()
+                if (receipt.status === 1) {
+                    console.log(`deployed lockup for ${info.beneficiary}`)
+                    await new Promise(r => setTimeout(r, 30000));
+                    continue
+                }
             }
         } catch (e) {
             console.log(`failed to deploy lockup for ${info.beneficiary}`)
+            console.log(e)
         }
     }
     let events: any[] = await lockupVaultFactory.queryFilter('VaultCreated')
@@ -77,7 +90,7 @@ async function launchVaults(lockupVaultFactory: any, lockupCsvFilename: string) 
     for (let i = 0; i < events.length; i++) {
         let beneficiary = events[i].args.beneficiary
         let employeeVaultAddress = events[i].args.vault
-        fs.writeFileSync('investorOutput.csv', '\n' + beneficiary + ',' + employeeVaultAddress, {
+        fs.writeFileSync('output/investorOutput.csv', '\n' + beneficiary + ',' + employeeVaultAddress, {
             encoding: 'utf8',
             flag: 'a+'
         })
@@ -105,10 +118,10 @@ async function processInputs(filename: string) {
             // console.log(beneficiary, amounts, timestamps)
             lockupInfos.push({beneficiary, amounts, timestamps})
         } catch (e) {
-            console.log(investorData[i])
+            console.log(e, investorData[i])
         }
-        console.log(lockupInfos[1])
     }
+    // console.log(lockupInfos)
 }
 
 // async function testInitialize(vaultAddressA32: string) {
@@ -120,22 +133,25 @@ async function processInputs(filename: string) {
 //     await vault.delegate(await wallet.getAddress())
 // }
 
-// async function checkClaiming(vaultAddressA32: string) {
-//     let provider: ethers.providers.BaseProvider = new ethers.providers.JsonRpcProvider(RPCURL)
-//     let wallet: ethers.Wallet = new ethers.Wallet(signerPK, provider)
-//     let ecox = new ethers.Contract(TOKEN, ecoXABI.abi, wallet)
-//     let initialbal = await ecox.balanceOf(await wallet.getAddress())
-//     console.log(initialbal.toString())
-//     let vault = new ethers.Contract(vaultAddressA32, lockupVaultABI.abi, wallet)
-//     let vaultbal = await ecox.balanceOf(vaultAddressA32)
-//     console.log(vaultbal.toString())
-//     let tx = await vault.claim()
-//     tx = await tx.wait()
-//     console.log(tx.status)
-//     await new Promise(r => setTimeout(r, 20000));
-//     vaultbal = await ecox.balanceOf(vaultAddressA32)
-//     console.log(vaultbal.toString())
-// }
+async function checkClaiming(vaultAddress: string) {
+    let provider: ethers.providers.BaseProvider = new ethers.providers.JsonRpcProvider(RPCURL)
+    let wallet: ethers.Wallet = new ethers.Wallet(signerPK, provider)
+
+    let policy = new ethers.Contract(POLICY, policyABI.abi, wallet)
+    let ecoX = new ethers.Contract(await policy.policyFor(ID_ECOX), ecoxABI.abi, wallet)
+
+    let initialbal = await ecoX.balanceOf(await wallet.getAddress())
+    console.log(initialbal.toString())
+    let vault = new ethers.Contract(vaultAddress, lockupVaultABI.abi, wallet)
+    let vaultbal = await ecoX.balanceOf(vaultAddress)
+    console.log(vaultbal.toString())
+    let tx = await vault.claim()
+    tx = await tx.wait()
+    console.log(tx.status)
+    await new Promise(r => setTimeout(r, 20000));
+    vaultbal = await ecoX.balanceOf(vaultAddress)
+    console.log(vaultbal.toString())
+}
 
 // async function stake() {
 //     let provider: ethers.providers.BaseProvider = new ethers.providers.JsonRpcProvider(RPCURL)
@@ -158,19 +174,14 @@ async function processInputs(filename: string) {
 // }
 
 
-async function checkBeneficiary() {
+async function checkBeneficiary(vaultAddress: string) {
     let provider: ethers.providers.BaseProvider = new ethers.providers.JsonRpcProvider(RPCURL)
     let wallet: ethers.Wallet = new ethers.Wallet(signerPK, provider)
-    let vaultAddress = '0xF2227f0c27929139Ace55E45569FAE2f963d6A44'
 
     let vault = new ethers.Contract(vaultAddress, lockupVaultABI.abi, wallet)
     console.log(await vault.beneficiary())
 }
 
-// checkBeneficiary().catch((error) => {
-//     console.error(error)
-//     process.exitCode = 1
-// })
 // test('0xF2e05c7687D78D11D8667130bD2d128C4854A874').catch((error) => {
 //     console.error(error)
 //     process.exitCode = 1
@@ -179,4 +190,9 @@ async function checkBeneficiary() {
 //     console.error(error)
 //     process.exitCode = 1
 // })
-fetchInvestorInfo('input/investors2.csv')
+
+// processInputs('input/investorsGoerli.csv')
+// deployVaultFactory()
+// launchVaults('0x698C470792e518388dA9e8F0648A6476B49BDB71', 'input/investorsGoerli.csv')
+// checkBeneficiary('0x42dF61714c9d2fbc1BA76278FdEBb123b0A1906b')
+// checkClaiming('0x42dF61714c9d2fbc1BA76278FdEBb123b0A1906b')
